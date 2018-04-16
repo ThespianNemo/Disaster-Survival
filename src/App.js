89abfe0457e421733a6e4283ad5207de95ddb7ea
Preston Cropp <pkcropp@gmail.com>
2018-04-16 11:51:04 -0500

    edited sound code and CSS components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import scenes from './scenes.json';
 import Sound from 'react-sound';
+import CitySounds from './Audio/City-of-Dread_Looping.mp3'
 
 class App extends Component {
   constructor() {
@@ -10,19 +11,33 @@ class App extends Component {
     this.state = {
       scene_num: 0
     }
+    
+    this.audio = new Audio(CitySounds);
+  }
+
+  componentDidMount = () => {
+    this.audio.play();
   }
 
   nextScene = (s) => {
+    if ([40,41,42,43].indexOf(s) > -1){
+      this.audio.pause();
+    }else if(s == 0 || s == 1){
+      this.audio.play();
+    }
     //changes scene_num in state to whatever gets passed in
     this.setState({ scene_num: s });
   }
 
   render() {
+    {/*url (string): The url of the sound to play.
+    playStatus (Sound.status.{PLAYING,STOPPED,PAUSED}): The current sound playing status. Change it in successive renders to play, stop, pause and resume the sound.*/}
     return (
 
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to Disaster Survival!</h1>
+          {/* <Sound url="./Audio/City-of-Dread_Looping.mp3" playStatus="PLAYING" />; */}
         </header>
 
         {/*Contains the disaster history>*/}
